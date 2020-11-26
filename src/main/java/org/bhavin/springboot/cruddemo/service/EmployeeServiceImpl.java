58a81de34870366d2eb108e78669f16ef5c88c6f
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-26 19:30:54 +0530

    Refactored code to replace Hibernate with JPA in DAO

--- src/main/java/org/bhavin/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/org/bhavin/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bhavin.springboot.cruddemo.dao.EmployeeDAO;
 import org.bhavin.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +15,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeDAO employeeDAO;
 	
 	@Autowired
-	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
 		employeeDAO = theEmployeeDAO;
 	}
 	
